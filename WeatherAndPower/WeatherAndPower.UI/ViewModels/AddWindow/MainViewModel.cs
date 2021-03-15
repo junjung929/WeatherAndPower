93b6a0c50bd68de98d9f5ba18561f3f11a256e62
Jun Jung <jun.jung@tuni.fi>
2021-03-15 10:29:05 +0200

    Initial setup for integrating Fingrid with AddWindow.Added switch control for power and weather input views.

---
+++ WeatherAndPower/WeatherAndPower.UI/ViewModels/AddWindow/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WeatherAndPower.UI.Commands;
+
+namespace WeatherAndPower.UI.ViewModels.AddWindow
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private BaseViewModel _selectedViewModel = new PowerInputViewModel();
+
+        public MainViewModel()
+        {
+            UpdateViewCommand = new UpdateViewCommand(this);
+        }
+
+
+        public ICommand UpdateViewCommand { get; set; }
+
+
+        public BaseViewModel SelectedViewModel
+        {
+            get { return _selectedViewModel; }
+            set 
+            { 
+                _selectedViewModel = value;
+                OnPropertyChanged(nameof(SelectedViewModel));
+            }
+        }
+    }
+}
