746a271b0881114cb55d5bd979e7960d6f2c422b
Jun Jung <jun.jung@tuni.fi>
2021-03-15 05:23:17 +0200

    Fix indentation

--- WeatherAndPower/WeatherAndPower.Data/Fingrid.cs
+++ WeatherAndPower/WeatherAndPower.Data/Fingrid.cs
@@ -58,7 +58,7 @@ namespace WeatherAndPower.Data
             if (format == "xml")
             {
                 IData singleData = ParseXMLToSingleData(variableId, body);
-                
+
                 Console.WriteLine(singleData.Value);
                 return singleData;
             }
@@ -168,11 +168,11 @@ namespace WeatherAndPower.Data
             ));
         }
 
-        private static IData ParseXMLToSingleData(Power.PowerTypes variableId,string XMLString)
+        private static IData ParseXMLToSingleData(Power.PowerTypes variableId, string XMLString)
         {
             var doc = new XmlDocument();
             doc.LoadXml(XMLString);
-            
+
             var evt = doc.SelectSingleNode("event");
             Console.WriteLine(evt);
             string val = evt.SelectSingleNode("value").InnerText;
@@ -210,7 +210,7 @@ namespace WeatherAndPower.Data
                     Console.WriteLine($"{startTime} {endTime} {val}");
                 }
             }
-            return new DataSeries(Power.powerTypes[variableId], DataFormat.Power , powerSeries);
+            return new DataSeries(Power.powerTypes[variableId], DataFormat.Power, powerSeries);
         }
     }
 }
