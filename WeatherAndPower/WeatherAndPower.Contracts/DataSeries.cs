aca4ff15bf47036ebb6726a36a8467c089ec1a0c
Saippua <ollikos92@gmail.com>
2021-03-16 20:37:08 +0200

    reverted to old datatype for DataSeries. added some more axis to graph

--- WeatherAndPower/WeatherAndPower.Contracts/DataSeries.cs
+++ WeatherAndPower/WeatherAndPower.Contracts/DataSeries.cs
@@ -14,25 +14,32 @@ namespace WeatherAndPower.Contracts
 
 		public DataFormat Format { get; set; }
 
-		public DateTime Start { get; set; }
+		//public DateTime Start { get; set; }
 
-		public DateTime End { get; set; }
+		//public DateTime End { get; set; }
 
-		public double[] Data { get; set; }
+		//public double[] Data { get; set; }
 
-		//public List<Tuple<DateTime, IData>> Series { get; set; }
-		public long[] Ticks {
-			get {
-				var startTicks = Start.Ticks;
-				var ticks = End.Ticks - startTicks;
-				var increment = ticks / Data.Length;
-				return Enumerable.Range(1, Data.Length).Select(i => i * increment).ToArray();
-			}
-		}
+		public List<Tuple<DateTime, IData>> Series { get; set; }
+		
+		//public long[] Ticks {
+		//	get {
+		//		var startTicks = Start.Ticks;
+		//		var ticks = End.Ticks - startTicks;
+		//		var increment = ticks / Data.Length;
+		//		return Enumerable.Range(1, Data.Length).Select(i => i * increment).ToArray();
+		//	}
+		//}
 
-		public DataSeries(string name, DataFormat format, DateTime start, DateTime end, double[] data)
+		//public DataSeries(string name, DataFormat format, DateTime start, DateTime end, double[] data)
+		//{
+		//	Name = name; Data = data; Start = start; End = end; Format = format;
+		//}
+		public DataSeries(string name, DataFormat format, List<Tuple<DateTime, IData>> series)
 		{
-			Name = name; Data = data; Start = start; End = end; Format = format;
+			Name = name;
+			Format = format;
+			Series = series;
 		}
 	}
 }
