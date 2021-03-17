909db3c3691708cd978785cf0f479ed605d7fd84
Jun Jung <jun.jung@tuni.fi>
2021-03-17 18:50:41 +0200

    Bind power categories from view model to combo box

--- WeatherAndPower/WeatherAndPower.UI/ViewModels/AddWindow/PowerInputViewModel.cs
+++ WeatherAndPower/WeatherAndPower.UI/ViewModels/AddWindow/PowerInputViewModel.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WeatherAndPower.Contracts;
 
 namespace WeatherAndPower.UI.ViewModels.AddWindow
 {
     public class PowerInputViewModel : BaseViewModel
     {
+
+        public ObservableCollection<PowerType> PowerTypes { get; set; }
+        public PowerType SPowerType { get; set; }
+
+        public PowerInputViewModel()
+        {
+            PowerTypes = new ObservableCollection<PowerType>(PowerType.GetAll<PowerType>());
+        }
     }
 }
