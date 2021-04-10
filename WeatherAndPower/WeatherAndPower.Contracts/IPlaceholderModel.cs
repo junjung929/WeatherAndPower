d3ebba192ed5066ec2b1f6ed7c4a4fde5bc28842
Saippua <ollikos92@gmail.com>
2021-04-10 12:24:59 +0300

    completed rebase

--- WeatherAndPower/WeatherAndPower.Contracts/IPlaceholderModel.cs
+++ WeatherAndPower/WeatherAndPower.Contracts/IPlaceholderModel.cs
@@ -16,6 +16,8 @@ namespace WeatherAndPower.Contracts
 
 		void PlaceholderAction5();
 
+		void SaveChart();
+
 		void AddPowerDataToPlotAction(PowerType powerType, DateTime startTime, DateTime endTime, string plotName);
 
 		void AddWeatherGraphAction(string cityName, string parameters, DateTime startTime, DateTime endTime, string plotName, WeatherType.ParameterEnum parameterType);
