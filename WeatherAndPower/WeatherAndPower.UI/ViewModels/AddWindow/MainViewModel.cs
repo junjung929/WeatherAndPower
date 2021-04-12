0024f00a08ca64111504298f5210388356858e3e
Jun Jung <jun.jung@tuni.fi>
2021-04-13 00:26:51 +0300

    #11 #12 Format date time pickers

--- WeatherAndPower/WeatherAndPower.UI/ViewModels/AddWindow/MainViewModel.cs
+++ WeatherAndPower/WeatherAndPower.UI/ViewModels/AddWindow/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -78,21 +79,73 @@ namespace WeatherAndPower.UI.ViewModels.AddWindow
         public DateTime StartTime
         {
             get { return _startTime; }
-            set { _startTime = value; NotifyPropertyChanged("StartTime"); }
+            set
+            {
+                if (DateTime.Compare(value, DateTimeMin) < 0)
+                {
+                    System.Windows.MessageBox.Show("The minimum range of date time is "
+                        + DateTimeMin.ToString("ddd dd'/'MM'/'yyyy HH:mm:ss", new CultureInfo("en-US")));
+                    _startTime = DateTimeMin;
+                }
+                else if (DateTime.Compare(value, DateTimeMax) > 0)
+                {
+                    System.Windows.MessageBox.Show("The maximum range of date time is "
+                       + DateTimeMax.ToString("ddd dd'/'MM'/'yyyy HH:mm:ss", new CultureInfo("en-US")));
+                    _startTime = DateTimeMax;
+                }
+                else _startTime = value;
+                NotifyPropertyChanged("StartTime");
+            }
         }
 
         public DateTime EndTime
         {
             get { return _endTime; }
-            set { _endTime = value; NotifyPropertyChanged("EndTime"); }
+            set
+            {
+                if (DateTime.Compare(value, DateTimeMin) < 0)
+                {
+                    System.Windows.MessageBox.Show("The minimum range of date time is "
+                        + DateTimeMin.ToString("ddd dd'/'MM'/'yyyy HH:mm:ss", new CultureInfo("en-US")));
+                    _endTime = DateTimeMin;
+                }
+                else if (DateTime.Compare(value, DateTimeMax) > 0)
+                {
+                    System.Windows.MessageBox.Show("The maximum range of date time is "
+                        + DateTimeMax.ToString("ddd dd'/'MM'/'yyyy HH:mm:ss", new CultureInfo("en-US")));
+                    _endTime = DateTimeMax;
+                }
+                else _endTime = value;
+                NotifyPropertyChanged("EndTime");
+            }
         }
 
-        public DateTime DateTimeMin { get; set; } = DateTime.Today.AddYears(-2);
-        public DateTime DateTimeMax { get; set; } = DateTime.Today.AddMonths(2).AddTicks(-1);
+        public DateTime DefaultDateTimeMin { get; private set; } = DateTime.Today.AddYears(-2);
+        public DateTime DefaultDateTimeMax { get; private set; } = DateTime.Today.AddMonths(2).AddTicks(-1);
 
+        private DateTime _dateTimeMin;
+        private DateTime _dateTimeMax;
+        public DateTime DateTimeMin
+        {
+            get { return _dateTimeMin; }
+            set { _dateTimeMin = value; NotifyPropertyChanged("DateTimeMin"); }
+        }
+        public DateTime DateTimeMax
+        {
+            get { return _dateTimeMax; }
+            set { _dateTimeMax = value; NotifyPropertyChanged("DateTimeMax"); }
+        }
+
+
+        public void SetDateTimeMinMaxToDefault()
+        {
+            DateTimeMin = DefaultDateTimeMin;
+            DateTimeMax = DefaultDateTimeMax;
+        }
         public string PlotName { get; set; }
         public MainViewModel(PlaceholderViewModel viewModel)
         {
+            SetDateTimeMinMaxToDefault();
             UpdateViewCommand = new UpdateViewCommand(this);
             UpdateDateTimeCommand = new UpdateDateTimeCommand(this);
             AddPlotCommand = new AddPlotCommand(this, viewModel);
