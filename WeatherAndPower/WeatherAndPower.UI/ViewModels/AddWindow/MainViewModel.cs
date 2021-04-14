b47bfc3c753add4d5908d2d1e862e1505e292db5
Jun Jung <jun.jung@tuni.fi>
2021-04-15 01:54:55 +0300

    #12 Moved datetime feature to separate view and viewmodel, and restructured PowerInputView. Implemented interval to PowerInputView

--- WeatherAndPower/WeatherAndPower.UI/ViewModels/AddWindow/MainViewModel.cs
+++ WeatherAndPower/WeatherAndPower.UI/ViewModels/AddWindow/MainViewModel.cs
@@ -18,7 +18,7 @@ namespace WeatherAndPower.UI.ViewModels.AddWindow
             Weather = 0x02
         }
 
-        private ViewModelBase _selectedViewModel;
+        private InputViewModelBase _selectedViewModel;
         private DataTypeEnum _dataType = (DataTypeEnum)0x01;
 
         public DataTypeEnum DataType
@@ -34,10 +34,9 @@ namespace WeatherAndPower.UI.ViewModels.AddWindow
 
         public ICommand UpdateViewCommand { get; set; }
         public ICommand AddPlotCommand { get; set; }
-        public ICommand UpdateDateTimeCommand { get; set; }
 
 
-        public ViewModelBase SelectedViewModel
+        public InputViewModelBase SelectedViewModel
         {
             get { return _selectedViewModel; }
             set
@@ -52,102 +51,10 @@ namespace WeatherAndPower.UI.ViewModels.AddWindow
             get { return Enum.GetValues(typeof(DataTypeEnum)); }
         }
 
-        private bool _isStartTimePickerEnabled = true;
-        private bool _isEndTimePickerEnabled = true;
-        public bool IsStartTimePickerEnabled
-        {
-            get { return _isStartTimePickerEnabled; }
-            set
-            {
-                _isStartTimePickerEnabled = value;
-                NotifyPropertyChanged(nameof(IsStartTimePickerEnabled));
-            }
-        }
-        public bool IsEndTimePickerEnabled
-        {
-            get { return _isEndTimePickerEnabled; }
-            set
-            {
-                _isEndTimePickerEnabled = value;
-                NotifyPropertyChanged(nameof(IsEndTimePickerEnabled));
-            }
-        }
-
-        private DateTime _startTime = DateTime.Now;
-        private DateTime _endTime = DateTime.Now;
-
-        public DateTime StartTime
-        {
-            get { return _startTime; }
-            set
-            {
-                if (DateTime.Compare(value, DateTimeMin) < 0)
-                {
-                    System.Windows.MessageBox.Show("The minimum range of start date time is "
-                        + DateTimeMin.ToString("ddd dd'/'MM'/'yyyy HH:mm:ss", new CultureInfo("en-US")));
-                    _startTime = DateTimeMin;
-                }
-                else if (DateTime.Compare(value, DateTimeMax) > 0)
-                {
-                    System.Windows.MessageBox.Show("The maximum range of start date time is "
-                       + DateTimeMax.ToString("ddd dd'/'MM'/'yyyy HH:mm:ss", new CultureInfo("en-US")));
-                    _startTime = DateTimeMax;
-                }
-                else _startTime = value;
-                NotifyPropertyChanged("StartTime");
-            }
-        }
-
-        public DateTime EndTime
-        {
-            get { return _endTime; }
-            set
-            {
-                if (DateTime.Compare(value, DateTimeMin) < 0)
-                {
-                    System.Windows.MessageBox.Show("The minimum range of end date time is "
-                        + DateTimeMin.ToString("ddd dd'/'MM'/'yyyy HH:mm:ss", new CultureInfo("en-US")));
-                    _endTime = DateTimeMin;
-                }
-                else if (DateTime.Compare(value, DateTimeMax) > 0)
-                {
-                    System.Windows.MessageBox.Show("The maximum range of end date time is "
-                        + DateTimeMax.ToString("ddd dd'/'MM'/'yyyy HH:mm:ss", new CultureInfo("en-US")));
-                    _endTime = DateTimeMax;
-                }
-                else _endTime = value;
-                NotifyPropertyChanged("EndTime");
-            }
-        }
-
-        public DateTime DefaultDateTimeMin { get; private set; } = DateTime.Today.AddYears(-2);
-        public DateTime DefaultDateTimeMax { get; private set; } = DateTime.Today.AddMonths(2).AddTicks(-1);
-
-        private DateTime _dateTimeMin;
-        private DateTime _dateTimeMax;
-        public DateTime DateTimeMin
-        {
-            get { return _dateTimeMin; }
-            set { _dateTimeMin = value; NotifyPropertyChanged("DateTimeMin"); }
-        }
-        public DateTime DateTimeMax
-        {
-            get { return _dateTimeMax; }
-            set { _dateTimeMax = value; NotifyPropertyChanged("DateTimeMax"); }
-        }
-
-
-        public void SetDateTimeMinMaxToDefault()
-        {
-            DateTimeMin = DefaultDateTimeMin;
-            DateTimeMax = DefaultDateTimeMax;
-        }
         public string PlotName { get; set; }
         public MainViewModel(PlaceholderViewModel viewModel)
         {
-            SetDateTimeMinMaxToDefault();
             UpdateViewCommand = new UpdateViewCommand(this);
-            UpdateDateTimeCommand = new UpdateDateTimeCommand(this);
             AddPlotCommand = new AddPlotCommand(this, viewModel);
             UpdateViewCommand.Execute(DataType);
         }
--- WeatherAndPower/WeatherAndPower.UI/ViewModels/AddWindow/PowerInputViewModel.cs
+++ WeatherAndPower/WeatherAndPower.UI/ViewModels/AddWindow/PowerInputViewModel.cs
@@ -10,54 +10,262 @@ using WeatherAndPower.UI.Commands;
 
 namespace WeatherAndPower.UI.ViewModels.AddWindow
 {
-    public class PowerInputViewModel : ViewModelBase
+    public class PowerInputViewModel : InputViewModelBase
     {
         MainViewModel ParentViewModel { get; set; }
+
+        private bool _isRealTime { get; set; } = false;
+        public bool IsRealTime
+        {
+            get { return _isRealTime; }
+            set
+            {
+                if (_isRealTime != value)
+                {
+                    _isRealTime = value;
+                    NotifyPropertyChanged("IsRealTime");
+                }
+            }
+        }
+
+        public ObservableCollection<PowerType.SourceEnum> PowerSources { get; set; }
+
+        private PowerType.SourceEnum _SelectedPowerSource { get; set; }
+
+        public PowerType.SourceEnum SelectedPowerSource
+        {
+            get { return _SelectedPowerSource; }
+            set
+            {
+                if (_SelectedPowerSource != value)
+                {
+                    _SelectedPowerSource = value;
+                    NotifyPropertyChanged("SelectedPowerSource");
+                }
+            }
+        }
+
+        private ObservableCollection<PowerType.ServiceEnum> _PowerServices { get; set; }
+        public ObservableCollection<PowerType.ServiceEnum> PowerServices
+        {
+            get { return _PowerServices; }
+            set
+            {
+                _PowerServices = value;
+                NotifyPropertyChanged("PowerServices");
+            }
+        }
+
+        private PowerType.ServiceEnum _SelectedPowerService { get; set; }
+        public PowerType.ServiceEnum SelectedPowerService
+        {
+            get { return _SelectedPowerService; }
+            set
+            {
+                _SelectedPowerService = value;
+                NotifyPropertyChanged("SelectedPowerService");
+            }
+        }
+
+        private ObservableCollection<PowerType.ParameterEnum> _PowerParameters { get; set; }
+        public ObservableCollection<PowerType.ParameterEnum> PowerParameters
+        {
+            get { return _PowerParameters; }
+            set
+            {
+                _PowerParameters = value;
+                NotifyPropertyChanged("PowerParameters");
+            }
+        }
+
+        private PowerType.ParameterEnum _SelectedPowerParameter { get; set; }
+        public PowerType.ParameterEnum SelectedPowerParameter
+        {
+            get { return _SelectedPowerParameter; }
+            set
+            {
+                _SelectedPowerParameter = value;
+                NotifyPropertyChanged("SelectedPowerParameter");
+            }
+        }
 
         public ObservableCollection<PowerType> PowerTypes { get; set; }
         private PowerType selectedPowerType { get; set; }
         public PowerType SPowerType
         {
-            get
-            {
-                return selectedPowerType;
-            }
+            get { return selectedPowerType; }
             set
             {
-                Console.WriteLine(value);
                 selectedPowerType = value;
                 NotifyPropertyChanged("SPowerType");
-
-                if (value == null) return;
-                //ParentViewModel.IsStartTimePickerEnabled = true;
-                //ParentViewModel.IsEndTimePickerEnabled = true;
-                ParentViewModel.SetDateTimeMinMaxToDefault();
-
-                if (value.Equals(PowerType.EConsumForecast24H)
-                    || value.Equals(PowerType.EProdPrediction24H))
-                {
-                    ParentViewModel.UpdateDateTimeCommand.Execute("n24h");
-                    ParentViewModel.DateTimeMax = ParentViewModel.EndTime;
-                }
-                else if (value.Equals(PowerType.SolarPowerGenerationForecast)
-                    || value.Equals(PowerType.WindPowerGenerationForecast))
-                {
-                    ParentViewModel.UpdateDateTimeCommand.Execute("n36h");
-                    ParentViewModel.DateTimeMax = ParentViewModel.EndTime;
-                }
-                else if (value.ParameterType.Equals(PowerType.ParameterEnum.Observation)
-                    || value.ParameterType.Equals(PowerType.ParameterEnum.RealTime))
-                {
-                    ParentViewModel.UpdateDateTimeCommand.Execute("");
-                    ParentViewModel.DateTimeMax = ParentViewModel.EndTime;
-                }
             }
         }
+
+        private ObservableCollection<Interval> _intervals { get; set; } = new ObservableCollection<Interval>()
+        {
+            new Interval(3),
+            new Interval(30),
+            new Interval(60),
+            new Interval(360),
+            new Interval(720),
+            new Interval(1440),
+            new Interval(1440*7),
+            new Interval(1440*30),
+        };
+
+        public ObservableCollection<Interval> Intervals
+        {
+            get { return _intervals; }
+            set { _intervals = value; NotifyPropertyChanged("Intervals"); }
+        }
+
+        private Interval _selectedInterval { get; set; }
+        public Interval SelectedInterval
+        {
+            get { return _selectedInterval; }
+            set { _selectedInterval = value; NotifyPropertyChanged("SelectedInterval"); }
+        }
+
+        private void UpdatePowerServices()
+        {
+            var selectableTypes = PowerTypes.ToList().FindAll(powerType => powerType.Source == SelectedPowerSource);
+            var selectableServices = selectableTypes.Select(selectableType => selectableType.Service).Distinct();
+            PowerServices = new ObservableCollection<PowerType.ServiceEnum>(selectableServices);
+            SelectedPowerService = PowerServices.First();
+        }
+
+        private void UpdatePowerParameters()
+        {
+            var selectableTypes = PowerTypes.ToList().FindAll(powerType =>
+            powerType.Source == SelectedPowerSource
+            && powerType.Service == SelectedPowerService);
+            var selectableParameters = selectableTypes.Select(selectableType => selectableType.ParameterType).Distinct();
+            PowerParameters = new ObservableCollection<PowerType.ParameterEnum>(selectableParameters);
+            SelectedPowerParameter = PowerParameters.First();
+        }
+
+        public override void OnDateTimeUpdated(string dateTime)
+        {
+        }
+
+        public void UpdateSelectedPowerType()
+        {
+
+            SPowerType = PowerTypes.ToList().Find(powerType =>
+                powerType.Source == SelectedPowerSource
+                && powerType.Service == SelectedPowerService
+                && powerType.ParameterType == SelectedPowerParameter);
+
+            Console.WriteLine(SPowerType);
+        }
+        public void UpdateRealTime()
+        {
+            if (SelectedPowerParameter == PowerType.ParameterEnum.RealTime)
+            {
+                IsRealTime = true;
+            }
+            else
+            {
+                IsRealTime = false;
+            }
+        }
+
+        public override void UpdateDateTimeMinMax()
+        {
+            if (SelectedPowerParameter == PowerType.ParameterEnum.RealTime ||
+                SelectedPowerParameter == PowerType.ParameterEnum.Observation)
+            {
+                DateTimeViewModel.UpdateDateTimeMinMax(DateTimeViewModel.DefaultDateTimeMin, DateTime.Now);
+            }
+            else
+            {
+                DateTimeViewModel.UpdateDateTimeMinMax(DateTimeViewModel.DefaultDateTimeMin,
+                    DateTimeViewModel.DefaultDateTimeMax);
+
+            }
+        }
+
+        int _minInterval { get; set; } = 3;
+        public void UpdateIntervals()
+        {
+            Console.WriteLine("interval " + SPowerType.Interval);
+            int minInterval = SPowerType.Interval;
+
+            if (minInterval != _minInterval)
+            {
+                _minInterval = minInterval;
+                var intervals = new ObservableCollection<Interval>();
+                Intervals.ToList().ForEach(interval =>
+                {
+                    if (interval.Value < minInterval)
+                    {
+                        intervals.Add(new Interval(interval.Value) { IsEnabled = false });
+                    }
+                    else
+                    {
+                        intervals.Add(new Interval(interval.Value));
+                    }
+                });
+                Intervals = intervals;
+                SelectedInterval = SelectedInterval.Value < minInterval ? Intervals.ToList().Find(interval => interval.Value >= minInterval) : SelectedInterval;
+            }
+        }
+
+
+
+        public void OnUpdateSelectedPowerSource()
+        {
+            Console.WriteLine("Power source " + SelectedPowerSource);
+            UpdatePowerServices();
+            UpdatePowerParameters();
+            UpdateRealTime();
+            UpdateSelectedPowerType();
+            UpdateDateTimeMinMax();
+            UpdateIntervals();
+        }
+
+        public void OnUpdateSelectedPowerService()
+        {
+            Console.WriteLine("Power service " + SelectedPowerSource);
+            UpdatePowerParameters();
+            UpdateRealTime();
+            UpdateSelectedPowerType();
+            UpdateDateTimeMinMax();
+            UpdateIntervals();
+        }
+
+        public void OnUpdateSelectedPowerParameter()
+        {
+            Console.WriteLine("Power parameter " + SelectedPowerParameter);
+            UpdateRealTime();
+            UpdateSelectedPowerType();
+            UpdateDateTimeMinMax();
+            UpdateIntervals();
+        }
+
+        public ICommand UpdatePowerSourceCommand { get; set; }
+        public ICommand UpdatePowerServiceCommand { get; set; }
+        public ICommand UpdatePowerParameterCommand { get; set; }
 
         public PowerInputViewModel(MainViewModel viewModel)
         {
             ParentViewModel = viewModel;
             PowerTypes = new ObservableCollection<PowerType>(PowerType.GetAll<PowerType>());
+            PowerSources = new ObservableCollection<PowerType.SourceEnum>(Enum.GetValues(typeof(PowerType.SourceEnum)).Cast<PowerType.SourceEnum>());
+            SelectedPowerSource = PowerType.SourceEnum.All;
+            UpdatePowerServices();
+            UpdatePowerParameters();
+            UpdateRealTime();
+            UpdateSelectedPowerType();
+            UpdateIntervals();
+
+            UpdatePowerSourceCommand = new RelayCommand(
+                () => OnUpdateSelectedPowerSource());
+            UpdatePowerServiceCommand = new RelayCommand(
+                () => OnUpdateSelectedPowerService());
+            UpdatePowerParameterCommand = new RelayCommand(
+                () => OnUpdateSelectedPowerParameter());
         }
+
     }
 }
