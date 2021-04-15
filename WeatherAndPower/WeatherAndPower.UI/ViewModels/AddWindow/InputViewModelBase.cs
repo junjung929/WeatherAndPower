2ab76f0cd2790e7c626c76de74dd149321f81412
Jun Jung <jun.jung@tuni.fi>
2021-04-15 17:56:30 +0300

    Fix namespace

--- WeatherAndPower/WeatherAndPower.UI/ViewModels/AddWindow/InputViewModelBase.cs
+++ WeatherAndPower/WeatherAndPower.UI/ViewModels/AddWindow/InputViewModelBase.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WeatherAndPower.UI.ViewModels.AddWindow
+﻿namespace WeatherAndPower.UI
 {
     public class InputViewModelBase : ViewModelBase
     {
