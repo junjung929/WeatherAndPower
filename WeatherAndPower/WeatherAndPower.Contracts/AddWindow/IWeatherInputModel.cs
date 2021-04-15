5dd755903d32b7b100a4253bd48e753c64224ffb
Jun Jung <jun.jung@tuni.fi>
2021-04-15 18:09:31 +0300

    Fix WeatherInput MVVM pattern

--- WeatherAndPower/WeatherAndPower.Contracts/AddWindow/IWeatherInputModel.cs
+++ WeatherAndPower/WeatherAndPower.Contracts/AddWindow/IWeatherInputModel.cs
@@ -9,6 +9,11 @@ namespace WeatherAndPower.Contracts
     public interface IWeatherInputModel
     {
         List<WeatherType> WeatherTypes { get; }
+        List<Interval> Intervals { get; }
+
         List<WeatherType> GetUpdatedWeatherTypes(WeatherType.ParameterEnum weatherParameter);
+        List<Interval> GetUpdatedIntervals(int minInterval);
+
+        IDateTimeInputModel CreateDateTimeInputModel();
     }
 }
--- WeatherAndPower/WeatherAndPower.Core/AddWindow/WeatherInputModel.cs
+++ WeatherAndPower/WeatherAndPower.Core/AddWindow/WeatherInputModel.cs
@@ -11,11 +11,45 @@ namespace WeatherAndPower.Core
     {
         public List<WeatherType> WeatherTypes { get; } = WeatherType.GetAll<WeatherType>().ToList();
 
+        public List<Interval> Intervals { get; } = new List<Interval>()
+        {
+            new Interval(5),
+            new Interval(10),
+            new Interval(30),
+            new Interval(60),
+            new Interval(360),
+            new Interval(720),
+            new Interval(1440),
+            new Interval(1440*7),
+            new Interval(1440*30)
+        };
+        public IDateTimeInputModel CreateDateTimeInputModel()
+        {
+            throw new NotImplementedException();
+        }
+
         public List<WeatherType> GetUpdatedWeatherTypes(WeatherType.ParameterEnum weatherParameter)
         {
             return WeatherTypes.FindAll(weatherType =>
                 weatherType.ParameterType
                 == weatherParameter);
+        }
+
+        public List<Interval> GetUpdatedIntervals(int minInterval)
+        {
+            var intervals = new List<Interval>();
+            Intervals.ForEach(interval =>
+            {
+                if (interval.Value < minInterval)
+                {
+                    intervals.Add(new Interval(interval.Value) { IsEnabled = false });
+                }
+                else
+                {
+                    intervals.Add(new Interval(interval.Value));
+                }
+            });
+            return intervals;
         }
     }
 }
