4d7e2bd1e2c33b5c274803f042c5f9980c087fc4
Jun Jung <jun.jung@tuni.fi>
2021-04-16 22:14:49 +0300

    Updating DateTimeInput

---
+++ WeatherAndPower/WeatherAndPower.Contracts/AddWindow/IDateTimeRange.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAndPower.Contracts
+{
+    public interface IDateTimeRange
+    {
+        string Name { get; set; }
+        string Description { get; set; }
+        string Value { get; set; }
+        bool IsEnabled { get; set; }
+        bool IsSelected { get; set; }
+    }
+}
---
+++ WeatherAndPower/WeatherAndPower.Core/AddWindow/DateTimeRange.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherAndPower.Contracts;
+
+namespace WeatherAndPower.Core
+{
+    class DateTimeRange : AbstractModel, IDateTimeRange
+    {
+        private string _Name { get; set; }
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                _Name = value; NotifyPropertyChanged("Name");
+            }
+        }
+
+        private string _Description { get; set; }
+        public string Description
+        {
+            get { return _Description; }
+            set { _Description = value; NotifyPropertyChanged("Description"); }
+        }
+
+        private string _Value { get; set; }
+        public string Value
+        {
+            get { return _Value; }
+            set { _Value = value; NotifyPropertyChanged("Value"); }
+        }
+
+        private bool _IsEnabled { get; set; } = true;
+        public bool IsEnabled
+        {
+            get { return _IsEnabled; }
+            set { _IsEnabled = value; NotifyPropertyChanged("IsEnabled"); }
+        }
+
+        private bool _IsSelected { get; set; } = false;
+        public bool IsSelected
+        {
+            get { return _IsSelected; }
+            set { _IsSelected = value; NotifyPropertyChanged("IsSelected"); }
+        }
+
+        public DateTimeRange(string name, string description, string value)
+        {
+            Name = name;
+            Description = description;
+            Value = value;
+        }
+    }
+}
