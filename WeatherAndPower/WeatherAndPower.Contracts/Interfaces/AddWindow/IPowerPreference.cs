c8d25fca09e1c2ecbc69c89750d0330b998a8ef9
Jun Jung <jun.jung@tuni.fi>
2021-04-17 19:09:58 +0300

    Adding preference interfaces

---
+++ WeatherAndPower/WeatherAndPower.Contracts/Interfaces/AddWindow/IPowerPreference.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAndPower.Contracts
+{
+    public interface IPowerPreference : IPreference
+    {
+        PowerType.SourceEnum PowerSource { get; set; }
+        PowerType.ServiceEnum PowerService { get; set; }
+        PowerType.ParameterEnum PowerParameter { get; set; }
+    }
+}
---
+++ WeatherAndPower/WeatherAndPower.Contracts/Interfaces/AddWindow/IPreference.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static WeatherAndPower.Contracts.IAddWindowModel;
+
+namespace WeatherAndPower.Contracts
+{
+    public interface IPreference
+    {
+        DataTypeEnum DataType { get; set; }
+
+
+        DateTime StartTime { get; set; }
+        DateTime EndTime { get; set; }
+        string PlotName { get; set; }
+        int Interval { get; set; }
+    }
+}
